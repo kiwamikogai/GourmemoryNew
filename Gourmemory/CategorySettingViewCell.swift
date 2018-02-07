fafc43e60c977e8ca365af233c6be9b11c15df67
小飼きわみ <kiwamikogai@me.com>
2018-02-07 22:24:55 +0900

    ランできなくて開発できないです！

--- Gourmemory/CategorySettingViewCell.swift
+++ Gourmemory/CategorySettingViewCell.swift
@@ -17,7 +17,6 @@ class CategorySettingViewCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
         
-        
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
